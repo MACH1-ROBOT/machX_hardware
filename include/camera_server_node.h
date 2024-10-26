a0922b8f47cc5afa8ceb9cb16b3368c8a16e1681
Julian Rendon <julianrendon514@gmail.com>
2024-10-25 20:08:00 -0400

    MH-02: Update camera server to start and stop

--- include/camera_server_node.h
+++ include/camera_server_node.h
@@ -5,16 +5,23 @@
 #include <std_msgs/msg/float32.hpp>
 
 constexpr int LUMINOSITY_PUB_TIMER_MS = 1000;
+constexpr char CAMERA_SERVER_NODE_NAME[] = "camera_server_node";
 
 class CameraServerNode : public rclcpp::Node, protected camera_driver::Camera
 {
   public:
     CameraServerNode();
 
+    CameraServerNode(std::string name, std::string type, int index, int fps);
+
+    ~CameraServerNode();
+
   private:
     float luminosityVal = 0;
     rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr luminosityPub_;
     rclcpp::TimerBase::SharedPtr timer_;
 
-    void publishLuminosityValue();
+    void InitializeCameraServer();
+    void Start();
+    void Stop();
 };
--- src/camera_server_node.cpp
+++ src/camera_server_node.cpp
@@ -1,25 +1,47 @@
 #include "camera_server_node.h"
 
-CameraServerNode::CameraServerNode() : Node("camera_server_node"), Camera()
+CameraServerNode::CameraServerNode() : Node(CAMERA_SERVER_NODE_NAME), Camera()
 {
-    luminosityPub_ = this->create_publisher<std_msgs::msg::Float32>("luminosity_value", 10);
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(LUMINOSITY_PUB_TIMER_MS),
-                                     std::bind(&CameraServerNode::publishLuminosityValue, this));
-    RCLCPP_INFO(this->get_logger(), "%s::Setting up camera server again.", __func__);
+    InitializeCameraServer();
 }
 
-void CameraServerNode::publishLuminosityValue()
+CameraServerNode::CameraServerNode(std::string name, std::string type, int index, int fps)
+    : Node(CAMERA_SERVER_NODE_NAME), Camera(name, type, index, fps)
 {
+    InitializeCameraServer();
+}
+
+CameraServerNode::~CameraServerNode() { Stop(); }
+
+void CameraServerNode::InitializeCameraServer()
+{
+    RCLCPP_INFO_ONCE(this->get_logger(), "%s::%s::Setting up camera server.",
+                     CAMERA_SERVER_NODE_NAME, __func__);
+    luminosityPub_ = this->create_publisher<std_msgs::msg::Float32>("luminosity_value", 10);
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(LUMINOSITY_PUB_TIMER_MS),
+                                     std::bind(&CameraServerNode::Start, this));
+}
+
+void CameraServerNode::Start()
+{
+    RCLCPP_INFO_ONCE(this->get_logger(), "%s::%s::Sever started.", CAMERA_SERVER_NODE_NAME,
+                     __func__);
     auto luminosityMsg = std_msgs::msg::Float32();
     luminosityMsg.data = luminosityVal;
     luminosityPub_->publish(luminosityMsg);
     luminosityVal++;
 }
 
+void CameraServerNode::Stop()
+{
+    RCLCPP_INFO_ONCE(this->get_logger(), "%s::%s::Server stopped.", CAMERA_SERVER_NODE_NAME,
+                     __func__);
+}
+
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<CameraServerNode>();
+    auto node = std::make_shared<CameraServerNode>("RPi4", "USB", 0, 30);
     rclcpp::spin(node);
     return 0;
 }
