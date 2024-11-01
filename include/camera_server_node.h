cf5908a95cbb5904a61badd3566c00173a1edebf
Julian A Rendon <julianrendon514@gmail.com>
2024-11-01 14:42:49 -0400

    MH-02: Update camera server node to publish frame image.

--- include/camera_server_node.h
+++ include/camera_server_node.h
@@ -6,7 +6,8 @@
 #include <sensor_msgs/msg/compressed_image.hpp>
 #include <std_msgs/msg/float32.hpp>
 
-constexpr int LUMINOSITY_PUB_TIMER_MS = 1000;
+// constexpr int LUMINOSITY_PUB_TIMER_MS = 1000;
+constexpr int CAMERA_FPS_MS = 33;
 constexpr char CAMERA_SERVER_NODE_NAME[] = "camera_server_node";
 
 class CameraServerNode : public rclcpp::Node, protected camera_driver::Camera
@@ -18,9 +19,12 @@ class CameraServerNode : public rclcpp::Node, protected camera_driver::Camera
 
   private:
     rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr luminosityPub_;
+    rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr imageCompressedPub_;
     rclcpp::TimerBase::SharedPtr timer_;
 
     void InitializeServer();
     void Start();
     void Stop();
+    void PublishLuminosity(const cv::Mat &frame);
+    void PublishImage(const cv::Mat &frame);
 };
--- src/camera_server_node.cpp
+++ src/camera_server_node.cpp
@@ -19,8 +19,11 @@ CameraServerNode::~CameraServerNode()
 void CameraServerNode::InitializeServer()
 {
     RCLCPP_INFO(this->get_logger(), "%s()::Setting up camera server.", __func__);
-    luminosityPub_ = this->create_publisher<std_msgs::msg::Float32>("luminosity_value", 10);
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(LUMINOSITY_PUB_TIMER_MS),
+    luminosityPub_ = this->create_publisher<std_msgs::msg::Float32>("/luminosity_value", 10);
+    imageCompressedPub_ =
+        this->create_publisher<sensor_msgs::msg::CompressedImage>("/camera/image/compressed", 10);
+
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(CAMERA_FPS_MS),
                                      std::bind(&CameraServerNode::Start, this));
 }
 
@@ -29,14 +32,47 @@ void CameraServerNode::Start()
     RCLCPP_INFO_ONCE(this->get_logger(), "%s()::Sever started.", __func__);
 
     auto frame = CaptureFrame();
-    if (!frame)
+    if (frame.has_value())
+    {
+        PublishImage(frame.value());
+        PublishLuminosity(frame.value());
+    }
+    else
     {
         RCLCPP_ERROR(this->get_logger(), "%s()::Failed to capture frame.", __func__);
         Stop();
-        return;
     }
+}
 
-    auto luminosity = camera_utils::GetLuminosity(*frame);
+void CameraServerNode::Stop()
+{
+    RCLCPP_INFO_ONCE(this->get_logger(), "%s()::Stopping server.", __func__);
+    rclcpp::shutdown();
+}
+
+void CameraServerNode::PublishImage(const cv::Mat &frame)
+{
+    cv_bridge::CvImage cv_image;
+    cv_image.header.stamp = this->now();
+    cv_image.header.frame_id = "camera_frame";
+    cv_image.encoding = sensor_msgs::image_encodings::BGR8;
+    cv_image.image = frame;
+
+    auto compressed_image = std::make_shared<sensor_msgs::msg::CompressedImage>();
+    compressed_image->header = cv_image.header;
+    compressed_image->format = "jpeg";
+    cv::imencode(".jpg", cv_image.image, compressed_image->data);
+
+    imageCompressedPub_->publish(*compressed_image);
+}
+
+void CameraServerNode::PublishLuminosity(const cv::Mat &frame)
+{
+    // TODO: Remove once newer version of camera driver pkg is implemented.
+    // NOTE: GetLuminosity only accepts non-const variables
+    cv::Mat non_const_frame = frame.clone();
+
+    auto luminosity = camera_utils::GetLuminosity(non_const_frame);
     if (luminosity)
     {
         auto luminosityMsg = std_msgs::msg::Float32();
@@ -49,12 +85,6 @@ void CameraServerNode::Start()
     {
         RCLCPP_WARN(this->get_logger(), "%s()::Failed to get luminosity from the frame.", __func__);
     }
-}
-
-void CameraServerNode::Stop()
-{
-    RCLCPP_INFO_ONCE(this->get_logger(), "%s()::Stopping server.", __func__);
-    rclcpp::shutdown();
 }
 
 int main(int argc, char **argv)
